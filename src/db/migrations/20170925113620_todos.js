a37c6cbf76ba8124d16a09371a58fee55b7765af
benjaminpettus <benjaminpettus@gmail.com>
2017-09-25 17:39:08 -0700

    add modal created, card structure created

--- src/db/migrations/20170925113620_todos.js
+++ src/db/migrations/20170925113620_todos.js
@@ -2,6 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable( 'todos', ( table ) => {
     table.increments()
+    table.string('title')
     table.string('content')
   })
 };
