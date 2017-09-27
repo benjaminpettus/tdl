368f244118a7bf996d0305fb67c2f3e7b04f07bd
benjaminpettus <benjaminpettus@gmail.com>
2017-09-26 17:07:11 -0700

    fetch is working WIP db query

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,12 +1,28 @@
 $(document).ready(function(){
   console.log('we in this bih')
 
-  $('#modal1').modal()
+  // $('#modal1').modal()
 
-  const addContent = $('#addTodo')
+
   const addSubmit = document.querySelector('.addButton')
   addSubmit.addEventListener('click', () => {
-    console.log('clickin it')
+    const ee = document.getElementById('addTodo').value
+    newPost(ee)
   })
+
+
+  const newPost = (formValue) => {
+    console.log(formValue)
+    fetch('http://localhost:3000/new', {
+      method: 'post',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        content: formValue
+      })
+    })
+  }
+
 
 })
--- src/server.js
+++ src/server.js
@@ -12,13 +12,20 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', ( request, response ) => {
   response.render('index')
 })
 
-app.post('/', (request, response) => {
+app.get('/new', ( request, response ) => {
+  response.render('new')
+})
 
+
+app.post('/new', (request, response) => {
+  const { content } = request.body
+  console.log('content from route >>',content)
   response.render('index')
 })
 
@@ -31,7 +38,7 @@ app.post('/', (request, response) => {
 // })
 
 app.use((request, response) => {
-  res.status(404).render('not_found')
+  response.status(404).render('not-found')
 })
 
 
